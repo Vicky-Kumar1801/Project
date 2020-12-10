9488dfec03652fca14e441c7f48890b2cc865a6d
Vicky-Kumar1801 <75785404+Vicky-Kumar1801@users.noreply.github.com>
2020-12-10 16:16:34 +0530

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,186 @@
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+        <style>
+            .navbari {
+                width:240px;
+                background-color:red;
+                height:750px;
+                
+            }
+            a{
+                color:white;
+            }
+            hr {
+                color:white;
+            }
+
+            .mdl {
+                float:right;
+                margin-top:-33%;
+                margin-right:60%;
+            }
+            .mdl2 {
+                float:right;
+                margin-top:-33%;
+                margin-right:30%;
+            }
+            .mdl3 {
+                float:right;
+                margin-top:-33%;
+                margin-left:40%;
+            }
+            button{
+                border:none;
+            }
+        </style>
+</head>
+<body>
+<div class="navbari">
+<ul class="nav flex-column">
+
+<li class="nav-item"> 
+        <div class="dropdown">
+            <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Tania Andrew
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </li>
+    <hr>
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Dashboard</a>
+  </li>
+    <li class="nav-item"> 
+        <div class="dropdown">
+            <button style="border:none;" class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Components
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <div class="dropdown">
+            <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Forms
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <div class="dropdown">
+            <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Tables
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <div class="dropdown">
+            <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Maps
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Widgets</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Charts</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Calendar</a>
+  </li>
+
+  <li class="nav-item">
+        <div class="dropdown">
+            <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                Pages
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </li>
+
+    <li class="nav-item">
+    <a class="nav-link" href="#">Documentation</a>
+  </li>
+</ul>
+</div>
+
+<div class="mdl">
+    <div class="card" style="width: 18rem;">
+        <img src="cozy.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Cozy 5 Stars Apartment</h5>
+            <p class="card-text">This place is close to Barceloneta Beach and Bus stop just 2 min by walk and near to "Naviglio Where you can enjoy the main night life in Barcelona.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><b> $899/Night</b></li>
+        </ul>
+    </div>
+
+    
+</div>
+
+
+<div class="mdl2">
+    <div class="card" style="width: 18rem;">
+        <img src="office.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Office Studio</h5>
+            <p class="card-text">This place is close to Metro station and bus stop just 2 min by walk and near to Naviglio where you can enjoy the night life in London.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><b> $1.119/Night</b></li>
+        </ul>
+    </div>
+
+    
+</div>
+
+<div class="mdl3">
+    <div class="card" style="width: 18rem;">
+        <img src="castle.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Beautiful castle</h5>
+            <p class="card-text">This place is close to Metro station and Bus stop just 2 min by walk and near to "Naviglio Where you can enjoy the main night life in Milan.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><b> $459/Night</b></li>
+        </ul>
+    </div>
+
+    
+</div>
+</body>
+</head>
